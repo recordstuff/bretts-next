f637d1adea4c837e57f65673e74e1699f0f679eb
Brett Drake <recordstuff@hotmail.com>
2026-08-09 10:50:57 -0500

    Simplify attribute definition names

--- src/models/InventoryItemAttributeDefinitionDetail.ts
+++
@@ -1,7 +0,0 @@
-import { InventoryItemAttributeDataType } from './InventoryItemAttributeDataType'
-
-export interface InventoryItemAttributeDefinitionDetail {
-    Guid: string
-    Name: string
-    DataType: InventoryItemAttributeDataType
-}
